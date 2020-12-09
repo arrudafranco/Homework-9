1b73b8e8f124e1927d5aa857b6c7910f690e486f
arrudafranco <arrudafranco@uchicago.edu>
2020-12-09 04:52:22 -0600

    Updates on stop words.

--- COVID&religion_topics.R
+++ COVID&religion_topics.R
@@ -84,3 +84,62 @@ ggplot(top_terms, aes(term, beta, fill = topic)) +
   scale_x_reordered() +
   facet_wrap(~ topic, scales = "free", ncol = 3) +
   coord_flip()
+
+
+topical_stop_words <- data.frame(word = c("senhor", "deus", "igrejas", "igreja", "aleluia",
+                                          "glória", "jesus", "verso", "capítulo", "bispo"))
+full_stop_words <- union(PT_stop_words, topical_stop_words)
+
+# remove stop words or n-grams beginning or ending with stop word
+corpus_full_stop_words <- corpus_tokens %>%
+  # separate ngrams into separate columns
+  separate(col = word,
+           into = c("word1", "word2", "word3", "word4", "word5"),
+           sep = " ") %>%
+  # find last word
+  mutate(last = if_else(ngram == 5, word5,
+                        if_else(ngram == 4, word4,
+                                if_else(ngram == 3, word3,
+                                        if_else(ngram == 2, word2, word1))))) %>%
+  # remove tokens where the first or last word is a stop word
+  filter(word1 %in% full_stop_words$word |
+           last %in% full_stop_words$word) %>%
+  select(ngram, token_id)
+
+# convert to dtm
+corpus_filtered_dtm <- corpus_tokens %>%
+  # remove stop word tokens
+  anti_join(corpus_full_stop_words) %>%
+  # get count of each token in each document
+  count(id, word) %>%
+  # create a document-term matrix with all features and tf weighting
+  cast_dtm(document = id, term = word, value = n) %>%
+  removeSparseTerms(sparse = .999)
+
+# Joining, by = c("ngram", "token_id")
+
+# remove documents with no terms remaining
+corpus_filtered_dtm <- corpus_filtered_dtm[unique(corpus_filtered_dtm$i),]
+
+corpus_filtered_lda12 <- LDA(corpus_filtered_dtm, k = 9, control = list(seed = 123))
+
+# A LDA_VEM topic model with 9 topics.
+
+corpus_filtered_lda12_td <- tidy(corpus_filtered_lda12)
+
+top_terms_filtered <- corpus_filtered_lda12_td %>%
+  group_by(topic) %>%
+  top_n(5, beta) %>%
+  ungroup() %>%
+  arrange(topic, -beta)
+
+top_terms_filtered %>%
+  mutate(topic = factor(topic),
+         term = reorder_within(term, beta, topic))
+
+ggplot(top_terms_filtered, aes(term, beta, fill = topic)) +
+  geom_bar(alpha = 0.8, stat = "identity", show.legend = FALSE) +
+  scale_fill_gradientn(colors = brewer.pal(6, "Dark2")) +
+  scale_x_reordered() +
+  facet_wrap(~ topic, scales = "free", ncol = 3) +
+  coord_flip()
